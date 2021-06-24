7d2af63452dba00c5aef293afbe939b8e413964e
Rybak123 <80932718+Rybak123@users.noreply.github.com>
2021-06-24 18:19:44 -0400

    CalendarioMaso

--- Cliente/src/app/calender/calender.component.ts
+++ Cliente/src/app/calender/calender.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit,Input,Output,EventEmitter} from '@angular/core';
-import { CalendarOptions } from '@fullcalendar/angular';
+import { CalendarOptions, EventInput, FullCalendarComponent } from '@fullcalendar/angular';
 import esLocale from '@fullcalendar/core/locales/es-us';
 import { ClaseMensaje } from './mensaje';
+import {HttpClient} from '@angular/common/http';
+import { ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'app-calender',
@@ -10,29 +13,62 @@ import { ClaseMensaje } from './mensaje';
 })
 export class CalenderComponent implements OnInit,ClaseMensaje {
 
+
+  @ViewChild('calendar') calendarComponent: FullCalendarComponent;
+
+  
   public verMensaje1(){
     alert("sadsad");
   }
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
   mensaje: String="asdsad";
+
+  calendarVisible = true;
+
+  calendarWeekends = true;
+
+  calendarEvents: EventInput[] = [
+    { title: 'Event Now', date: new Date() }
+  ];
 
   ngOnInit(): void {
     
-  
+    this.currentUserId = sessionStorage.getItem('currentUser');   
+
+     this.http.post(`http://localhost:3000/usuarios/${this.currentUserId}`,{}).toPromise().then(data =>{
+      let aux = JSON.stringify(data);         
+      this.currentUser = JSON.parse(aux);
+
+      this.http.post('http://localhost:3000/gruposId/', this.currentUser.grupos).subscribe(data => {
+      let userData = JSON.stringify(data);
+      this.grupos = JSON.parse(userData);
+
+      var tareasGrupos:any[]=[];
+
+      this.grupos.forEach(element => {
+        element.tareas.forEach(element1 => {
+          tareasGrupos.push(element1);
+          });
+        });
+
+ 
+      tareasGrupos.forEach(element => {
+        var mydate = new Date(element.endDate.trim());
+
+        var taskName= element.titulo;
+        var date1 = (mydate.getFullYear()+"-"+mydate.getMonth()+"-"+mydate.getDay());
+        this.calendarEvents = this.calendarEvents.concat({ 
+          title: taskName,
+          start: date1
+        })
+      });
+      });
+    });  
   }
-  locales = [esLocale];
-  calendarOptions: CalendarOptions = {
-    initialView: 'dayGridMonth',
-    locale: esLocale,
-    dateClick: this.handleDateClick.bind(this), // bind is important!
-    events: [
-      { title: 'Tarea 1', date: '2021-05-01' },
-      { title: 'Tarea 2', date: '2021-05-01' },
-      { title: 'Recordatorio 1', date: '2021-05-11' },
-      { title: 'Tarea 3', date: '2021-05-16' }
-    ]
-  };
+ 
+  grupos:any;
+
   handleDateClick(arg) {
 
   }
@@ -42,4 +78,17 @@ export class CalenderComponent implements OnInit,ClaseMensaje {
   hello:(hello:any)=>{alert(hello)};
   @Output()
   onSomething = new EventEmitter<string>();
+
+  currentUserId;
+  currentUser;
+ 
+
+  
 }
+
+
+
+
+
+
+
